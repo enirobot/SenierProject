977c26e7d0e8f41ef74cf57d38ed8910f0935d9d
parkdh9018 <parkdh9018@naver.com>
2018-11-24 15:57:12 +0900

    게임 진행중2

--- news/src/main/java/com/team/news/Controller/GameController.java
+++ news/src/main/java/com/team/news/Controller/GameController.java
@@ -1,4 +1,42 @@
 package com.team.news.Controller;
 
+import com.team.news.Repository.MainNewsListRepository;
+import com.team.news.Repository.NewsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
 public class GameController {
+    private final NewsRepository newsRepository;
+    private final MongoTemplate mongoTemplate;
+
+    private final MainNewsListRepository mainNewsListRepository;
+
+    @Autowired
+    public GameController(NewsRepository newsRepository, MainNewsListRepository mainNewsListRepository
+            , MongoTemplate mongoTemplate) {
+        this.newsRepository = newsRepository;
+        this.mongoTemplate = mongoTemplate;
+        this.mainNewsListRepository = mainNewsListRepository;
+    }
+
+
+    @GetMapping("/game")
+    public String main(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        String str = "야호";
+
+        request.setAttribute("yaho",str);
+
+        return "game";
+    }
+
+
+
 }
