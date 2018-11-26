0f889a3f083ab55031f71ec76a51b2596c0d6da2
DESKTOP-TR8J8B2\ckdrm <ckdrmsdl9999@naver.com>
2018-11-26 18:03:35 +0900

    no message

---
+++ news/src/main/java/com/team/news/Form/BubbleFormAndDate.java
@@ -0,0 +1,32 @@
+package com.team.news.Form;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class BubbleFormAndDate {
+    private String date;
+    private List<BubbleForm> bubbleitems = new ArrayList<BubbleForm>();
+    private String group;
+
+    public void addBubbleitems(BubbleForm bubbleForm) {
+        this.bubbleitems.add(bubbleForm);
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public List<BubbleForm> getBubbleitems() {
+        return bubbleitems;
+    }
+
+    public String getDate() {return date;}
+
+    public String toString() {return "BubbleFormAndDate : date="+date+", group="+group;}
+}
