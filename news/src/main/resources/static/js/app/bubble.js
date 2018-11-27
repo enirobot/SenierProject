d1ce9f11d0c44dff3f074de5bad25c410c252547
ckdrmckdrmsdl9999@naver.com <ckdrmsdl9999@naver.com>
2018-11-27 18:57:09 +0900

    no message

--- news/src/main/resources/static/js/app/bubble.js
+++ news/src/main/resources/static/js/app/bubble.js
@@ -1,60 +1,47 @@
-function initSankey(sankey_div){
-    console.log("initSankey : " + sankey_div);
-    var inputdata = [];
-    google.charts.load('current', {'packages':['sankey']});
+function initBubble(bubble_div){
+    console.log("initBubble : " + bubble_div);
+    var inputdata1 = [];
+    google.charts.load("current", {packages: ["corechart"]});
 
     $(document).ready(function(){
         $.ajax({
-            url: "/sankey_major_post",
+            url: "/bubble_post",
             type: "POST",
             dataType: "json",
             contentType: "application/json",
             data: JSON.stringify(null),
             success: function(data) {
+                inputdata1.push(['키워드','총가중치','기사수','감정수치','총댓글수']);
                 for (var i = 0; i < data.length; i++) {
-                    inputdata.push( [ data[i].source, data[i].destination, data[i].value ] );
+                    inputdata1.push( [data[i].word, data[i].totalWeight,data[i].counts,data[i].totalEmotionWeight,data[i].totalcommentCount]);
+                    console.log([data[i].word+data[i].totalWeight+" "+data[i].totalcommentCount+" "+data[i].totalEmotionWeight+" "+data[i].counts]);
                 }
+                var timeout1 = setInterval(function () {
+                    if (google.visualization !== undefined) {
+                        google.charts.setOnLoadCallback(drawChart1);
+                        clearInterval(timeout1);
+                    }
+                }, 300);
+                console.log(JSON.stringify(inputdata1));
 
-                google.charts.setOnLoadCallback(drawChart);
             }
         })
-
     });
 
+    function drawChart1() {
+        console.log(JSON.stringify(inputdata1));
+        var data = google.visualization.arrayToDataTable(inputdata1);
 
-    function drawChart() {
-        var data = new google.visualization.DataTable();
-        data.addColumn('string', 'From');
-        data.addColumn('string', 'To');
-        data.addColumn('number', 'Weight');
-        data.addRows(inputdata);
-
-        var colors = ['#a6cee3', '#b2df8a', '#fb9a99', '#fdbf6f',
-            '#cab2d6', '#ffff99', '#1f78b4', '#33a02c'];
-
-        // Sets chart options.
         var options = {
-            width: window.innerWidth,
-            height: window.innerHeight,
-            sankey: {
-                node: {
-                    colors: colors
-                },
-                link: {
-                    colorMode: 'gradient',
-                    colors: colors
-                }
-            }
+            title: 'Bubble chart(-부정적인키워드+긍정적인키워드)',
+            hAxis: {title: 'TotalWeight(가중치)'},
+            vAxis: {minValue:0,maxValue:15,title: 'Count(기사수)'},
+            colorAxis: {minValue:-10,maxValue:50,colors: ['yellow', 'red']},////////////////
+
+            bubble: {textStyle: {fontSize: 10}}
         };
 
-        // sankey_div.width = window.innerWidth;
-        // sankey_div.height = window.innerHeight;
-        //
-        // console.log(sankey_div.width + " : " + window.innerWidth);
-        // console.log(sankey_div.height + " : " + window.innerHeight);
-
-        // Instantiates and draws our chart, passing in some options.
-        var chart = new google.visualization.Sankey(sankey_div);
+        var chart = new google.visualization.BubbleChart(bubble_div);
         chart.draw(data, options);
     }
 
